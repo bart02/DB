2e70580bda4bf2faf7ca4739aeaa3681473d89d6
Artem Batalov <a@batalov.me>
2022-04-04 14:28:20 +0300

    .

--- lab7/ex1/excercise.sql
+++ lab7/ex1/excercise.sql
@@ -1,7 +1,9 @@
+-- number of items and sum
 SELECT bi."orderId", SUM(bi."quant") AS "items_count", SUM(bi."quant" * i."price") AS "sum" FROM "bucketItem" AS bi 
 JOIN "item" AS i ON i."id" = bi."itemId"
 GROUP BY bi."orderId";
 
+-- the most "rich" customer
 SELECT c.*, SUM(bi."quant" * i."price") FROM "customer" AS c
 JOIN "order" AS o ON c."id" = o."customerId"
 JOIN "bucketItem" AS bi ON c."id" = bi."orderId"
