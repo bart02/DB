d2befcf25f2227e318a12a235da46a541149e75b
Artem Batalov <a@batalov.me>
2022-04-04 14:24:26 +0300

    ,

--- ex1.sql
+++
@@ -1,4 +0,0 @@
-SELECT bi."orderId", SUM(bi."quant") AS "items_count", SUM(bi."quant" * i."price") AS "sum" FROM "bucketItem" AS bi 
-JOIN "item" AS i ON i."id" = bi."itemId"
-GROUP BY bi."orderId";
-
---
+++ lab7/ex1/ex1.sql
@@ -0,0 +1,9 @@
+SELECT bi."orderId", SUM(bi."quant") AS "items_count", SUM(bi."quant" * i."price") AS "sum" FROM "bucketItem" AS bi 
+JOIN "item" AS i ON i."id" = bi."itemId"
+GROUP BY bi."orderId";
+
+SELECT c.*, SUM(bi."quant" * i."price") FROM "customer" AS c
+JOIN "order" AS o ON c."id" = o."customerId"
+JOIN "bucketItem" AS bi ON c."id" = bi."orderId"
+JOIN "item" AS i ON i."id" = bi."itemId"
+GROUP BY c."id" ORDER BY SUM(bi."quant" * i."price") DESC LIMIT 1;
