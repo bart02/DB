9481521c1dbd86ab792a66f2d5040bb555e65132
Artem Batalov <a@batalov.me>
2022-04-04 14:18:35 +0300

    Create ex1.sql

---
+++ lab7/ex1.sql
@@ -0,0 +1,3 @@
+SELECT bi."orderId", SUM(bi."quant") AS "items_count", SUM(bi."quant" * i."price") AS "sum" FROM "bucketItem" AS bi 
+JOIN "item" AS i ON i."id" = bi."itemId"
+GROUP BY bi."orderId";
