4f7b22e66863473c2b5116303c97d23285b92c5b
Artem Batalov <a@batalov.me>
2022-04-04 14:17:17 +0300

    Create 2nf.sql

---
+++ lab7/2nf.sql
@@ -0,0 +1,28 @@
+CREATE TABLE "order" (
+  "id" integer NOT NULL,
+  "date" date NOT NULL,
+  "customerId" integer NOT NULL,
+  PRIMARY KEY("id")
+);
+
+CREATE TABLE "customer" (
+  "id" integer NOT NULL,
+  "name" text NOT NULL,
+  "city" text NOT NULL,
+  PRIMARY KEY("id")
+);
+
+CREATE TABLE "item" (
+  "id" integer NOT NULL,
+  "name" text NOT NULL,
+  "price" integer NOT NULL,
+  PRIMARY KEY("id")
+);
+
+CREATE TABLE "bucketItem" (
+  "id" integer NOT NULL,
+  "itemId" integer NOT NULL,
+  "orderId" integer NOT NULL,
+  "quant" integer NOT NULL,
+  PRIMARY KEY("id")
+);
